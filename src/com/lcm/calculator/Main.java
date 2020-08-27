8cba56f0d59725855b4d21027e09e88a8b9cc02c
Glenn Alon <glennalon216@aol.com>
2020-08-27 21:26:02 +0800

    edit

--- src/com/lcm/calculator/Main.java
+++ src/com/lcm/calculator/Main.java
@@ -1,6 +1,5 @@
 package com.lcm.calculator;
 
-
 import java.util.Scanner;
 
 public class Main {
@@ -8,14 +7,16 @@ public class Main {
     public static void main(String[] args) {
         int num1, num2, lcm;
         Scanner input = new Scanner(System.in);
+        System.out.println("Least Common Multiple Calculator");
         System.out.println("Enter the first number");
         num1 = Integer.parseInt(input.nextLine());
         System.out.println("Enter the second number");
         num2 = Integer.parseInt(input.nextLine());
-        lcm = (num1 > num2) ? num1 : num2;
+        lcm = Math.max(num1, num2);
         while(true) {
             if( lcm % num1 == 0 && lcm % num2 == 0 ) {
                 System.out.printf("The LCM of %d and %d is %d.", num1, num2, lcm);
+                System.out.println();
                 break;
             }
             ++lcm;
